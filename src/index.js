b774c1679414f1221317128fe138975ea85a76ea
Leonid Petrov <lp.msarchitect@gmail.com>
2020-04-29 15:32:11 +0300

    feat: add webpack config

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import keys from './keys.js';
+import Keyboard from './Keyboard.js';
+
+const layout = localStorage.layout ? localStorage.layout : 'eng';
+const keyboard = new Keyboard(keys.keyRows, keys.layoutMaps, layout);
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `<textarea id="output" rows=10 cols=85></textarea>${keyboard.html}<br><br><div>Переключение раскладки Alt+Shift</div>`
+);
+console.log('hello!');
