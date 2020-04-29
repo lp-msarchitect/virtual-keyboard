88bebcba88e46b205e34791da34c620ef6c17132
Leonid Petrov <lp.msarchitect@gmail.com>
2020-04-29 15:32:33 +0300

    del unuse files

--- index.js
+++
@@ -1,9 +0,0 @@
-import keys from './src/keys.js';
-import Keyboard from './src/Keyboard.js';
-
-const layout = localStorage.layout ? localStorage.layout : 'eng';
-const keyboard = new Keyboard(keys.keyRows, keys.layoutMaps, layout);
-document.body.insertAdjacentHTML(
-  'afterbegin',
-  `<textarea id="output" rows=10 cols=85></textarea>${keyboard.html}<br><br><div>Переключение раскладки Alt+Shift</div>`
-);
